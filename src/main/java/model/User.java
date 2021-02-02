2f2403b7900fb3f27409b23a06db82f24d8db8ba
bkc123 <chhantyalbinod@gmail.com>
2021-02-01 18:30:27 -0600

    added User and User repository class

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,5 +1,63 @@
 package model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table (name = "new_users")
 public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
+	private String fname;
+	private String lname;
+	private String email;
+	private Long contact;
+	public User() {
+		
+	}
+	
+	public User(String fname, String lname, String email, Long contact) {
+		super();
+		this.fname = fname;
+		this.lname = lname;
+		this.email = email;
+		this.contact = contact;
+	}
+
+	public String getFname() {
+		return fname;
+	}
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+	public String getLname() {
+		return lname;
+	}
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Long getContact() {
+		return contact;
+	}
+	public void setContact(Long contact) {
+		this.contact = contact;
+	}
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", fname=" + fname + ", lname=" + lname + ", email=" + email + ", contact=" + contact
+				+ "]";
+	}
+	
+	
 
 }
